f49f2f077783afaac9fabf27fd788cedad7291be
Antoine Aflalo <Belphemur@users.noreply.github.com>
2020-05-08 19:40:35 -0400

    Add Caching service

---
+++ AddictedProxy/Services/Caching/CachingService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Runtime.Caching;
+using Microsoft.Extensions.Caching.Memory;
+using MemoryCache = System.Runtime.Caching.MemoryCache;
+
+namespace AddictedProxy.Services.Caching
+{
+    public class CachingService : ICachingService
+    {
+        private readonly MemoryCache _memoryCache;
+
+        public CachingService(MemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
+        /// <summary>
+        /// Get or set in the cache
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="objBuilder"></param>
+        /// <param name="expiry">Does this entry expire ?</param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public T GetSet<T>(string key, Func<T> objBuilder, TimeSpan? expiry)
+        {
+            var inCache = _memoryCache.Get(key);
+            if (inCache != null)
+            {
+                return (T) inCache;
+            }
+
+            var toCache = objBuilder();
+            var cachePolicy = new CacheItemPolicy
+            {
+                AbsoluteExpiration = DateTimeOffset.Now + (expiry ?? TimeSpan.Zero),
+                RemovedCallback = async arguments =>
+                {
+                    switch (arguments.CacheItem.Value)
+                    {
+                        case IDisposable disposable:
+                            disposable.Dispose();
+                            break;
+                        case IAsyncDisposable disposable:
+                            await disposable.DisposeAsync();
+                            break;
+                    }
+                }
+            };
+            var cacheEntry = new CacheItem(key, toCache);
+            _memoryCache.Add(cacheEntry, cachePolicy);
+            return toCache;
+        }
+    }
+}
---
+++ AddictedProxy/Services/Caching/ICachingService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AddictedProxy.Services.Caching
+{
+    public interface ICachingService
+    {
+        /// <summary>
+        /// Get or set in the cache
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="objBuilder"></param>
+        /// <param name="expiry">Does this entry expire ?</param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        T GetSet<T>(string key, Func<T> objBuilder, TimeSpan? expiry);
+    }
+}
