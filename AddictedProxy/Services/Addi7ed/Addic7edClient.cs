cbd441f1b510450a0966c984563c5228c16bd100
Antoine Aflalo <Belphemur@users.noreply.github.com>
2020-05-08 19:40:43 -0400

    Starting app

---
+++ AddictedProxy/Services/Addi7ed/Addic7edClient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net.Http;
+
+namespace AddictedProxy.Services.Addi7ed
+{
+    public class Addic7edClient
+    {
+        private readonly HttpClient _httpClient;
+
+        public Addic7edClient(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+            _httpClient.BaseAddress = new Uri("https://www.addic7ed.com");
+        }
+        
+    }
+}
---
+++ AddictedProxy/Services/Proxy/ProxySelector.cs
@@ -0,0 +1,14 @@
+﻿using System.Net.Http;
+
+namespace AddictedProxy.Services.Proxy
+{
+    public class ProxySelector
+    {
+        private readonly HttpClient _httpClient;
+
+        public ProxySelector(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+    }
+}
