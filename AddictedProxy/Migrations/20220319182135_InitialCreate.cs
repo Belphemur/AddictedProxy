48ea687873354a0ae21da7a83a8e4e5b054666f5
Antoine Aflalo <Belphemur@users.noreply.github.com>
2022-03-19 16:11:49 -0400

    Add proper migrations

---
+++ AddictedProxy/Migrations/20220319182135_InitialCreate.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AddictedProxy.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TvShows",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TvShows", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Episodes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    TvShowId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Season = table.Column<int>(type: "INTEGER", nullable: false),
+                    Number = table.Column<int>(type: "INTEGER", nullable: false),
+                    Title = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Episodes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Episodes_TvShows_TvShowId",
+                        column: x => x.TvShowId,
+                        principalTable: "TvShows",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Subtitles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    EpisodeId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Version = table.Column<string>(type: "TEXT", nullable: false),
+                    Completed = table.Column<bool>(type: "INTEGER", nullable: false),
+                    HearingImpaired = table.Column<bool>(type: "INTEGER", nullable: false),
+                    Corrected = table.Column<bool>(type: "INTEGER", nullable: false),
+                    HD = table.Column<bool>(type: "INTEGER", nullable: false),
+                    DownloadUri = table.Column<string>(type: "TEXT", nullable: false),
+                    Language = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Subtitles", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Subtitles_Episodes_EpisodeId",
+                        column: x => x.EpisodeId,
+                        principalTable: "Episodes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Episodes_TvShowId",
+                table: "Episodes",
+                column: "TvShowId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Subtitles_EpisodeId",
+                table: "Subtitles",
+                column: "EpisodeId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Subtitles");
+
+            migrationBuilder.DropTable(
+                name: "Episodes");
+
+            migrationBuilder.DropTable(
+                name: "TvShows");
+        }
+    }
+}
---
+++ AddictedProxy/Migrations/20220319195835_IndexOnEpisode.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AddictedProxy.Migrations
+{
+    public partial class IndexOnEpisode : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Episodes_TvShowId",
+                table: "Episodes");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Episodes_TvShowId_Season_Number",
+                table: "Episodes",
+                columns: new[] { "TvShowId", "Season", "Number" },
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Episodes_TvShowId_Season_Number",
+                table: "Episodes");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Episodes_TvShowId",
+                table: "Episodes",
+                column: "TvShowId");
+        }
+    }
+}
