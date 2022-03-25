aeb3e273ef16d1621b804ce0d7954d9d5c8ee1f9
Antoine Aflalo <Belphemur@users.noreply.github.com>
2022-03-25 17:53:49 -0400

    Add usage of systemd

--- AddictedProxy/Program.cs
+++ AddictedProxy/Program.cs
@@ -13,6 +13,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Host.UseSystemd();
 
 new Startup().ConfigureServices(builder.Services);
 
