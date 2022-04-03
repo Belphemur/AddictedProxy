23f0f62c5f2745c3b6e0202fd083c9eff2c941f9
Antoine Aflalo <Belphemur@users.noreply.github.com>
2022-04-03 13:47:20 -0400

    Only update credential usage if the credentials were actually used

--- AddictedProxy/Model/Crendentials/CredsContainer.cs
+++ AddictedProxy/Model/Crendentials/CredsContainer.cs
@@ -10,17 +10,32 @@ namespace AddictedProxy.Model.Crendentials;
 public class CredsContainer : IAsyncDisposable
 {
     private readonly ICredentialsService _credentialsService;
+    private readonly AddictedUserCredentials _addictedUserCredentials;
+
+    private bool _isCredentialsCalled = false;
 
     public CredsContainer(ICredentialsService credentialsService, AddictedUserCredentials addictedUserCredentials)
     {
-        AddictedUserCredentials = addictedUserCredentials;
+        _addictedUserCredentials = addictedUserCredentials;
         _credentialsService = credentialsService;
     }
 
-    public AddictedUserCredentials AddictedUserCredentials { get; }
+    public AddictedUserCredentials AddictedUserCredentials
+    {
+        get
+        {
+            _isCredentialsCalled = true;
+            return _addictedUserCredentials;
+        }
+    }
 
     public async ValueTask DisposeAsync()
     {
+        if (!_isCredentialsCalled)
+        {
+            return;
+        }
+
         await _credentialsService.UpdateUsageCredentialsAsync(AddictedUserCredentials, default);
     }
 }
