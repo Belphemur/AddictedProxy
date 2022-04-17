0f6ea8f8c1affec754b0bba473bac78687aaec11
Antoine Aflalo <Belphemur@users.noreply.github.com>
2022-04-16 21:50:05 -0400

    Use nice library for Keyed locking
    Works better for our use case than having dangling semaphores

--- Locking/Lock.cs
+++ Locking/Lock.cs
@@ -1,16 +1,14 @@
-﻿namespace Locking;
+﻿using KeyedSemaphores;
+
+namespace Locking;
 
 public static class Lock<T>
 {
-    private static readonly LockDictionary Locks = LockDictionary.Instance;
-    private static readonly object CacheLock = new();
-
     public class Container : IDisposable
     {
-        private readonly SemaphoreSlim _semaphore;
-        private bool _hasLock;
+        private readonly IKeyedSemaphore _semaphore;
 
-        public Container(SemaphoreSlim semaphore)
+        public Container(IKeyedSemaphore semaphore)
         {
             _semaphore = semaphore;
         }
@@ -21,18 +19,14 @@ public static class Lock<T>
         /// <param name="timeSpan"></param>
         /// <param name="token"></param>
         /// <returns>True if got the lock before <see cref="timeSpan"/> expires</returns>
-        public async Task<bool> WaitAsync(TimeSpan timeSpan, CancellationToken token)
+        public Task<bool> WaitAsync(TimeSpan timeSpan, CancellationToken token)
         {
-            return _hasLock = await _semaphore.WaitAsync(timeSpan, token);
+            return _semaphore.WaitAsync(timeSpan, token);
         }
 
         public void Dispose()
         {
-            //Only release the lock if we had it
-            if (_hasLock)
-            {
-                _semaphore.Release();
-            }
+            _semaphore.Dispose();
         }
     }
 
@@ -43,16 +37,7 @@ public static class Lock<T>
     /// <returns></returns>
     public static Container GetNamedLock(string name)
     {
-        lock (CacheLock)
-        {
-            var lockKey = GetLockKey(name);
-            if (!Locks.ContainsKey(lockKey))
-            {
-                Locks.TryAdd(lockKey, new SemaphoreSlim(1, 1));
-            }
-
-            return new Container(Locks[lockKey]);
-        }
+        return new Container(KeyedSemaphore.GetOrCreate(name));
     }
 
     private static string GetLockKey(string key)
