bf37fdf44ecf9d7c63e125da0250478c71be5221
Antoine Aflalo <Belphemur@users.noreply.github.com>
2022-04-23 17:39:02 -0400

    Add rate limiting by IP

---
+++ AddictedProxy/Controllers/Bootstrap/BootstrapRateLimiting.cs
@@ -0,0 +1,25 @@
+﻿using AspNetCoreRateLimit;
+using InversionOfControl.Model;
+
+namespace AddictedProxy.Controllers.Bootstrap;
+
+public class BootstrapRateLimiting : IBootstrap, IBootstrapApp
+{
+    public void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddMemoryCache();
+        services.Configure<IpRateLimitOptions>(configuration.GetSection("IpRateLimiting"));
+
+        //load ip rules from appsettings.json
+        services.Configure<IpRateLimitPolicies>(configuration.GetSection("IpRateLimitPolicies"));
+
+        // inject counter and rules stores
+        services.AddInMemoryRateLimiting();
+        services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
+    }
+
+    public void ConfigureApp(IApplicationBuilder app)
+    {
+        app.UseIpRateLimiting();
+    }
+}
