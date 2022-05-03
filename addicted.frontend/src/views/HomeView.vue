435e4f193067fc189d45b711bec67b74cec6e23a
Antoine Aflalo <Belphemur@users.noreply.github.com>
2022-05-03 00:02:56 -0400

    deps(Frontend): Add Element +
    Will be used as main framework

--- addicted.frontend/src/views/HomeView.vue
+++ addicted.frontend/src/views/HomeView.vue
@@ -1,18 +1,11 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from "vue-class-component";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import { Vue } from "vue-class-component";
 
-@Options({
-  components: {
-    HelloWorld,
-  },
-})
 export default class HomeView extends Vue {}
 </script>
--- addicted.frontend/vue.config.js
+++ addicted.frontend/vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require("@vue/cli-service");
+const { ElementPlusResolver } = require("unplugin-vue-components/resolvers");
+
 module.exports = defineConfig({
   transpileDependencies: true,
+  configureWebpack: {
+    plugins: [
+      require("unplugin-vue-components/webpack")({
+        resolvers: [ElementPlusResolver()],
+      }),
+      require("unplugin-auto-import/webpack")({
+        resolvers: [ElementPlusResolver()],
+      }),
+    ],
+  },
 });
