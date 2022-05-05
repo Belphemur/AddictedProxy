243d9153a1a483cc18f9b9a2bcaece68a4fcc34c
Antoine Aflalo <Belphemur@users.noreply.github.com>
2022-05-05 18:53:38 -0400

    refactor(Search): Basic autocomplete search

---
+++ addicted.frontend/src/components/SearchComponent.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="mt-4">
+    <el-autocomplete
+      v-model="searchInput"
+      :fetch-suggestions="querySearch"
+      clearable
+      placeholder="Search for your episode: Wellington S01E01"
+      class="inline-input"
+      size="large"
+      :debounce="500"
+      :trigger-on-focus="false"
+    >
+      <template #prepend>
+        <el-select
+          v-model="languageSelect"
+          placeholder="Select"
+          style="width: 115px"
+        >
+          <el-option label="English" value="en" />
+        </el-select>
+      </template>
+    </el-autocomplete>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, watch } from "vue";
+
+const searchInput = ref("");
+const userLang = (navigator.language || navigator.userLanguage).split("-")[0];
+const languageSelect = ref(userLang);
+
+const querySearch = (query: string, cb: (param: unknown) => void) => {
+  console.log(query);
+  cb([{ value: "Hello World" }]);
+};
+</script>
+
+<style scoped></style>
--- addicted.frontend/src/views/HomeView.vue
+++ addicted.frontend/src/views/HomeView.vue
@@ -1,11 +1,5 @@
 <template>
-  <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+  <SearchComponent />
 </template>
 
-<script lang="ts">
-import { Vue } from "vue-class-component";
-
-export default class HomeView extends Vue {}
-</script>
+<script setup lang="ts"></script>
