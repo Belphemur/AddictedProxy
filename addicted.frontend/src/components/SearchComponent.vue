59340d8fe1738ceb8d593456bd1d5e9a14f78126
Antoine Aflalo <Belphemur@users.noreply.github.com>
2022-05-05 22:19:39 -0400

    feat(frontend::show): Search working to find a specific show

--- addicted.frontend/src/components/SearchComponent.vue
+++ addicted.frontend/src/components/SearchComponent.vue
@@ -19,20 +19,32 @@
           <el-option label="English" value="en" />
         </el-select>
       </template>
+      <template #default="{ item }">
+        <span class="name">{{ item.value }}</span>
+        <span v-if="item.seasons > 0"> ({{ item.seasons }})</span>
+      </template>
     </el-autocomplete>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, watch } from "vue";
+import { ref } from "vue";
+import { TvShowsApi, Configuration } from "@/api";
 
 const searchInput = ref("");
 const userLang = (navigator.language || navigator.userLanguage).split("-")[0];
 const languageSelect = ref(userLang);
+const api = new TvShowsApi(
+  new Configuration({ basePath: process.env.VUE_APP_API_PATH })
+);
 
-const querySearch = (query: string, cb: (param: unknown) => void) => {
-  console.log(query);
-  cb([{ value: "Hello World" }]);
+const querySearch = async (query: string, cb: (param: unknown) => void) => {
+  const searchResponse = await api.showsSearchPost({ query: query });
+  cb(
+    searchResponse.shows?.map((show) => {
+      return { value: show.name, id: show.name, seasons: show.nbSeasons };
+    })
+  );
 };
 </script>
 
