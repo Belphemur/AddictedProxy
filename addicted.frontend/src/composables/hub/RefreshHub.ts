77329506000a7e245dc37a737701d5b0d6b68330
Antoine Aflalo <Belphemur@users.noreply.github.com>
2022-05-11 21:30:15 -0400

    feat(Hub::frontend): Add frontend hub for refresh

---
+++ addicted.frontend/src/composables/hub/RefreshHub.ts
@@ -0,0 +1,48 @@
+import { HubConnectionBuilder, LogLevel } from "@microsoft/signalr";
+import { ShowDto } from "@/api";
+
+const connection = new HubConnectionBuilder()
+  .withUrl(process.env.VUE_APP_API_PATH + "/refresh")
+  .configureLogging(LogLevel.Information)
+  .withAutomaticReconnect()
+  .build();
+
+async function start() {
+  try {
+    await connection.start();
+    console.log("SignalR Connected.");
+  } catch (err) {
+    console.log(err);
+    setTimeout(start, 5000);
+  }
+}
+
+await start();
+
+export interface Progress {
+  showId: string;
+  progress: number;
+}
+
+export type ProgressHandler = (progress: Progress) => void;
+export type DoneHandler = (show: ShowDto) => void;
+
+export function sendRefreshAsync(showId: string) {
+  return connection.invoke("RefreshShow", showId);
+}
+
+export function onProgress(handler: ProgressHandler) {
+  connection.on("Progress", handler);
+}
+
+export function OffProgress(handler: ProgressHandler) {
+  connection.off("Progress", handler);
+}
+
+export function OnDone(handler: DoneHandler) {
+  connection.on("Done", handler);
+}
+
+export function OffDone(handler: DoneHandler) {
+  connection.off("Done", handler);
+}
