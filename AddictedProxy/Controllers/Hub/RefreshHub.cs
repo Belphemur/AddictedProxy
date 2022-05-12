bdd3f3bec555801e94fbc28d0c127bf7655f8275
Antoine Aflalo <Belphemur@users.noreply.github.com>
2022-05-12 11:10:02 -0400

    feat(Hub::Refresh::Unsubscribe): Be able to unsubscribe from specific show refresh

--- AddictedProxy/Controllers/Hub/RefreshHub.cs
+++ AddictedProxy/Controllers/Hub/RefreshHub.cs
@@ -25,7 +25,6 @@ public class RefreshHub : Hub<IRefreshClient>
     /// Trigger the refresh of a show
     /// </summary>
     /// <param name="showId"></param>
-    /// <param name="token"></param>
     public async Task RefreshShow(Guid showId)
     {
         await Groups.AddToGroupAsync(Context.ConnectionId, showId.ToString());
@@ -39,5 +38,15 @@ public class RefreshHub : Hub<IRefreshClient>
                              .Configure(job => job.Show = show)
                              .Build();
         _jobScheduler.ScheduleJob(job);
+    }
+
+    /// <summary>
+    /// Unsubscribe from getting refresh information about specific show
+    /// </summary>
+    /// <param name="showId"></param>
+    /// <returns></returns>
+    public Task UnsubscribeRefreshShow(Guid showId)
+    {
+        return Groups.RemoveFromGroupAsync(Context.ConnectionId, showId.ToString());
     }
 }
