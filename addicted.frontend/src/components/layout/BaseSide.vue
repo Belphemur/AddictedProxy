981b20f7f7ba73b13205a013e4444fd0aa78ac7c
Antoine Aflalo <Belphemur@users.noreply.github.com>
2022-05-14 14:58:21 -0400

    refactor(Ui): Improve the UI by affixing the menu and removing margin on the body

--- addicted.frontend/src/components/layout/BaseSide.vue
+++ addicted.frontend/src/components/layout/BaseSide.vue
@@ -1,32 +1,34 @@
 <template>
-  <el-aside width="200px" class="menu">
-    <el-scrollbar>
-      <el-menu :collapse="false" :router="true" :default-active="$route.path">
-        <img
-          :src="isDark ? transparent : black"
-          alt="gestdown logo"
-          width="180"
-        />
-        <el-menu-item-group>
-          <el-menu-item
-            v-for="route in $router.getRoutes()"
-            v-bind:key="route.path"
-            :route="route"
-            :index="route.path"
-            >{{ route.name }}
-          </el-menu-item>
-        </el-menu-item-group>
-        <el-menu-item h="full">
-          <el-switch
-            v-model="isDark"
-            size="large"
-            :active-icon="Moon"
-            :inactive-icon="Sunny"
+  <el-affix>
+    <el-aside width="200px" class="menu">
+      <el-scrollbar>
+        <el-menu :collapse="false" :router="true" :default-active="$route.path">
+          <img
+            :src="isDark ? transparent : black"
+            alt="gestdown logo"
+            width="180"
           />
-        </el-menu-item>
-      </el-menu>
-    </el-scrollbar>
-  </el-aside>
+          <el-menu-item-group>
+            <el-menu-item
+              v-for="route in $router.getRoutes()"
+              v-bind:key="route.path"
+              :route="route"
+              :index="route.path"
+              >{{ route.name }}
+            </el-menu-item>
+          </el-menu-item-group>
+          <el-menu-item h="full">
+            <el-switch
+              v-model="isDark"
+              size="large"
+              :active-icon="Moon"
+              :inactive-icon="Sunny"
+            />
+          </el-menu-item>
+        </el-menu>
+      </el-scrollbar>
+    </el-aside>
+  </el-affix>
 </template>
 
 <script lang="ts" setup>
