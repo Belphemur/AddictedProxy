67391e4948d13df87cec0b55b9a9cf9be5364f28
Antoine Aflalo <Belphemur@users.noreply.github.com>
2022-05-24 12:23:08 -0400

    refactor(TransactionManager): Use proper transaction manager for the upsert of episode

--- AddictedProxy.Database/Repositories/Shows/EpisodeRepository.cs
+++ AddictedProxy.Database/Repositories/Shows/EpisodeRepository.cs
@@ -3,6 +3,7 @@
 using System.Globalization;
 using AddictedProxy.Database.Context;
 using AddictedProxy.Database.Model.Shows;
+using AddictedProxy.Database.Transaction;
 using Microsoft.EntityFrameworkCore;
 using Z.BulkOperations;
 
@@ -13,11 +14,13 @@ namespace AddictedProxy.Database.Repositories.Shows;
 public class EpisodeRepository : IEpisodeRepository
 {
     private readonly EntityContext _entityContext;
+    private readonly ITransactionManager _transactionManager;
 
 
-    public EpisodeRepository(EntityContext entityContext)
+    public EpisodeRepository(EntityContext entityContext, ITransactionManager transactionManager)
     {
         _entityContext = entityContext;
+        _transactionManager = transactionManager;
     }
 
     /// <summary>
@@ -25,41 +28,33 @@ public class EpisodeRepository : IEpisodeRepository
     /// </summary>
     public async Task UpsertEpisodes(IEnumerable<Episode> episodes, CancellationToken token)
     {
-        var transaction = await _entityContext.Database.BeginTransactionAsync(token);
-        try
+        await using var transaction = await _transactionManager.BeginNestedAsync(token);
+        var enumerable = episodes as Episode[] ?? episodes.ToArray();
+        await _entityContext.Episodes.BulkMergeAsync(enumerable, options =>
         {
-            var enumerable = episodes as Episode[] ?? episodes.ToArray();
-            await _entityContext.Episodes.BulkMergeAsync(enumerable, options =>
+            options.IncludeGraph = true;
+            options.IncludeGraphOperationBuilder = operation =>
             {
-                options.IncludeGraph = true;
-                options.IncludeGraphOperationBuilder = operation =>
+                switch (operation)
                 {
-                    switch (operation)
-                    {
-                        case BulkOperation<Subtitle> bulkSub:
-                            bulkSub.IgnoreOnMergeUpdateExpression = subtitle => new { subtitle.Discovered, subtitle.StoragePath, subtitle.StoredAt, subtitle.DownloadCount, subtitle.UniqueId };
-                            bulkSub.ColumnPrimaryKeyExpression = subtitle => new { subtitle.EpisodeId, subtitle.Language, subtitle.Version };
-                            break;
-                        case BulkOperation<Episode> bulkEp:
-                            bulkEp.ColumnPrimaryKeyExpression = episode => new { episode.TvShowId, episode.Season, episode.Number };
-                            bulkEp.IgnoreOnMergeUpdateExpression = episode => episode.Discovered;
-                            break;
-                        case BulkOperation<TvShow> bulkShow:
-                            bulkShow.IsReadOnly = true;
-                            break;
-                    }
-                };
-            }, token);
+                    case BulkOperation<Subtitle> bulkSub:
+                        bulkSub.IgnoreOnMergeUpdateExpression = subtitle => new { subtitle.Discovered, subtitle.StoragePath, subtitle.StoredAt, subtitle.DownloadCount, subtitle.UniqueId };
+                        bulkSub.ColumnPrimaryKeyExpression = subtitle => new { subtitle.EpisodeId, subtitle.Language, subtitle.Version };
+                        break;
+                    case BulkOperation<Episode> bulkEp:
+                        bulkEp.ColumnPrimaryKeyExpression = episode => new { episode.TvShowId, episode.Season, episode.Number };
+                        bulkEp.IgnoreOnMergeUpdateExpression = episode => episode.Discovered;
+                        break;
+                    case BulkOperation<TvShow> bulkShow:
+                        bulkShow.IsReadOnly = true;
+                        break;
+                }
+            };
+        }, token);
 
-            await transaction.CommitAsync(token);
-        }
-        catch (Exception)
-        {
-            await transaction.RollbackAsync(token);
-            throw;
-        }
+        await transaction.CommitAsync(token);
     }
-    
+
     /// <summary>
     /// Get season episodes
     /// </summary>
@@ -69,10 +64,10 @@ public class EpisodeRepository : IEpisodeRepository
     public IAsyncEnumerable<Episode> GetSeasonEpisodesAsync(long tvShowId, int season)
     {
         return _entityContext.Episodes.Where(episode => episode.Season == season)
-                             .Where(episode => episode.TvShow.Id == tvShowId)
-                             .Include(episode => episode.TvShow)
-                             .Include(episode => episode.Subtitles)
-                             .ToAsyncEnumerable();
+            .Where(episode => episode.TvShow.Id == tvShowId)
+            .Include(episode => episode.TvShow)
+            .Include(episode => episode.Subtitles)
+            .ToAsyncEnumerable();
     }
 
     /// <summary>
@@ -85,10 +80,10 @@ public class EpisodeRepository : IEpisodeRepository
     public IAsyncEnumerable<Episode> GetSeasonEpisodesByLangAsync(long tvShowId, CultureInfo language, int season)
     {
         return _entityContext.Episodes.Where(episode => episode.Season == season)
-                             .Where(episode => episode.TvShow.Id == tvShowId)
-                             .Include(episode => episode.TvShow)
-                             .Include(episode => episode.Subtitles.Where(subtitle => subtitle.Language == language.EnglishName))
-                             .ToAsyncEnumerable();
+            .Where(episode => episode.TvShow.Id == tvShowId)
+            .Include(episode => episode.TvShow)
+            .Include(episode => episode.Subtitles.Where(subtitle => subtitle.Language == language.EnglishName))
+            .ToAsyncEnumerable();
     }
 
     /// <summary>
@@ -97,12 +92,12 @@ public class EpisodeRepository : IEpisodeRepository
     public Task<Episode?> GetEpisodeUntrackedAsync(long tvShowId, int season, int episodeNumber, CancellationToken token)
     {
         return _entityContext.Episodes
-                             .Where(episode => episode.Number == episodeNumber)
-                             .Where(episode => episode.Season == season)
-                             .Where(episode => episode.TvShow.Id == tvShowId)
-                             .Include(episode => episode.TvShow)
-                             .Include(episode => episode.Subtitles)
-                             .AsNoTracking()
-                             .FirstOrDefaultAsync(token);
+            .Where(episode => episode.Number == episodeNumber)
+            .Where(episode => episode.Season == season)
+            .Where(episode => episode.TvShow.Id == tvShowId)
+            .Include(episode => episode.TvShow)
+            .Include(episode => episode.Subtitles)
+            .AsNoTracking()
+            .FirstOrDefaultAsync(token);
     }
 }
