f8a26de44e9fa0f68c501383faa3c0be6f6112b8
Antoine Aflalo <Belphemur@users.noreply.github.com>
2022-05-29 10:29:37 -0400

    fix(Creds): Fix issue where not finding credentials throw exception.
    Fixes ADDICTEDPROXY-2M
    Fixes #1

--- AddictedProxy.Database/Repositories/Credentials/AddictedUserCredentialRepository.cs
+++ AddictedProxy.Database/Repositories/Credentials/AddictedUserCredentialRepository.cs
@@ -3,6 +3,7 @@
 using AddictedProxy.Database.Context;
 using AddictedProxy.Database.Model.Credentials;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 #endregion
 
@@ -11,17 +12,26 @@ namespace AddictedProxy.Database.Repositories.Credentials;
 public class AddictedUserCredentialRepository : IAddictedUserCredentialRepository
 {
     private readonly EntityContext _context;
+    private readonly ILogger<AddictedUserCredentialRepository> _logger;
 
-    public AddictedUserCredentialRepository(EntityContext context)
+    public AddictedUserCredentialRepository(EntityContext context, ILogger<AddictedUserCredentialRepository> logger)
     {
         _context = context;
+        _logger = logger;
     }
 
     public async Task<AddictedUserCredentials?> GetLeastUsedCredAsync(CancellationToken token)
     {
         try
         {
-            var min = await _context.AddictedUserCreds.MinAsync(credentials => credentials.Usage, token);
+            //Casting as nullable type to handle the case where the sequence contains no elements, it will return null instead of throw.
+            //keeping the catch in case of
+            var min = await _context.AddictedUserCreds.MinAsync(credentials => (int?)credentials.Usage, token);
+            if (min == null)
+            {
+                _logger.LogWarning("Couldn't find credential to use for querying");
+                return null;
+            }
             return await _context.AddictedUserCreds.Where(credentials => credentials.Usage <= min).FirstOrDefaultAsync(token);
         }
         catch (InvalidOperationException e) when (e.Message == "Sequence contains no elements.")
@@ -34,7 +44,14 @@ public class AddictedUserCredentialRepository : IAddictedUserCredentialRepositor
     {
         try
         {
-            var min = await _context.AddictedUserCreds.Where(credentials => credentials.DownloadExceededDate == null).MinAsync(credentials => credentials.DownloadUsage, token);
+            //Casting as nullable type to handle the case where the sequence contains no elements, it will return null instead of throw.
+            //keeping the catch in case of
+            var min = await _context.AddictedUserCreds.Where(credentials => credentials.DownloadExceededDate == null).MinAsync(credentials => (int?)credentials.DownloadUsage, token);
+            if (min == null)
+            {
+                _logger.LogWarning("Couldn't find credential to use for download");
+                return null;
+            }
             return await _context.AddictedUserCreds.Where(credentials => credentials.DownloadUsage <= min && credentials.DownloadExceededDate == null).FirstOrDefaultAsync(token);
         }
         catch (InvalidOperationException e) when (e.Message == "Sequence contains no elements.")
