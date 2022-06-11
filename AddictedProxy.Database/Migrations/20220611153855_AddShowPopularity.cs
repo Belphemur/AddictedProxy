b29ddeb1a05a3cf6d65b6f390bd7194c014bfbee
Antoine Aflalo <Belphemur@users.noreply.github.com>
2022-06-11 11:40:06 -0400

    refactor(ShowPopularity): Add show popularity to the db

---
+++ AddictedProxy.Database/Migrations/20220611153855_AddShowPopularity.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AddictedProxy.Database.Migrations
+{
+    public partial class AddShowPopularity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ShowPopularity",
+                columns: table => new
+                {
+                    TvShowId = table.Column<long>(type: "INTEGER", nullable: false),
+                    Language = table.Column<string>(type: "TEXT", nullable: false),
+                    RequestedCount = table.Column<long>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ShowPopularity", x => new { x.TvShowId, x.Language });
+                    table.ForeignKey(
+                        name: "FK_ShowPopularity_TvShows_TvShowId",
+                        column: x => x.TvShowId,
+                        principalTable: "TvShows",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ShowPopularity");
+        }
+    }
+}
