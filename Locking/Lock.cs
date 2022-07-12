a030c017cb3ca807ec0912ca78d6126b216df862
Antoine Aflalo <Belphemur@users.noreply.github.com>
2022-07-12 16:27:26 -0400

    fix(Locking): Fix deprecation of method

--- Locking/Lock.cs
+++ Locking/Lock.cs
@@ -15,7 +15,7 @@ public static class Lock<T>
     /// <returns></returns>
     public static ILockContainer GetNamedLock(string name)
     {
-        return new Container(KeyedSemaphore.GetOrCreate(GetLockKey(name)));
+        return new Container(KeyedSemaphore.Provide(GetLockKey(name)));
     }
 
     private static string GetLockKey(string key)
