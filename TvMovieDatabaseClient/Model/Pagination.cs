68919839229805b65ef6027f4165bde9f35acd03
Antoine Aflalo <Belphemur@users.noreply.github.com>
2022-07-30 14:33:19 -0400

    feat(TVDB): Provide information from TVDB

---
+++ TvMovieDatabaseClient/Model/Pagination.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace TvMovieDatabaseClient.Model;
+
+public record Pagination<T>(
+    [property: JsonPropertyName("page")] int Page,
+    [property: JsonPropertyName("results")]
+    IReadOnlyList<T> Results,
+    [property: JsonPropertyName("total_pages")]
+    int TotalPages,
+    [property: JsonPropertyName("total_results")]
+    int TotalResults
+);
