fe8b644f13931979d07dfdc69d72c8a4d1825b13
Antoine Aflalo <Belphemur@users.noreply.github.com>
2022-08-03 21:16:13 -0400

    refactor(JSON): Add json context for performance

---
+++ TvMovieDatabaseClient/Model/JsonContext.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+using TvMovieDatabaseClient.Model.Search;
+
+namespace TvMovieDatabaseClient.Model;
+
+[JsonSerializable(typeof(ShowData))]
+[JsonSerializable(typeof(Pagination<SearchResult>))]
+internal partial class JsonContext : JsonSerializerContext
+{
+}
