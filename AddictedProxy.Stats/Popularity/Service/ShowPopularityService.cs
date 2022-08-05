574e48c53dd1a2bd77aeb608b46b63a2d0441f84
Antoine Aflalo <Belphemur@users.noreply.github.com>
2022-08-05 14:39:33 -0400

    fix(Popularity): Fix failure to record popularity because of concurrency

--- AddictedProxy.Stats/Popularity/Service/ShowPopularityService.cs
+++ AddictedProxy.Stats/Popularity/Service/ShowPopularityService.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using AddictedProxy.Database.Context;
 using AddictedProxy.Database.Model.Stats;
 using AddictedProxy.Stats.Popularity.Model;
+using Locking;
 using Microsoft.EntityFrameworkCore;
 
 namespace AddictedProxy.Stats.Popularity.Service;
@@ -29,6 +30,13 @@ internal class ShowPopularityService : IShowPopularityService
     /// <returns></returns>
     public async Task RecordPopularityAsync(RecordPopularityPayload payload, CancellationToken cancellationToken)
     {
+        using var namedLock = Lock<ShowPopularityService>.GetNamedLock(payload.Show.Id.ToString());
+
+        if (!await namedLock.WaitAsync(TimeSpan.FromSeconds(10), cancellationToken))
+        {
+            return;
+        }
+
         var popularity = await _entityContext.ShowPopularity.FindAsync(new object?[] { payload.Show.Id, payload.Language.Name }, cancellationToken: cancellationToken);
         if (popularity == null)
         {
@@ -55,18 +63,18 @@ internal class ShowPopularityService : IShowPopularityService
     public IAsyncEnumerable<DownloadPopularity> GetTopDownloadPopularity(int limit = 10)
     {
         return _entityContext.Subtitles
-                             .Include(subtitle => subtitle.Episode.TvShow.Seasons)
-                             .GroupBy(subtitle => subtitle.Episode.TvShowId)
-                             .Select(grouping => new
-                             {
-                                 grouping.First().Episode.TvShow,
-                                 TotalDownloads = grouping.Sum(subtitle => subtitle.DownloadCount)
-                             })
-                             .OrderByDescending(arg => arg.TotalDownloads)
-                             .AsNoTracking()
-                             .Take(limit)
-                             .ToAsyncEnumerable()
-                             .Select(arg => new DownloadPopularity(arg.TvShow, arg.TotalDownloads));
+            .Include(subtitle => subtitle.Episode.TvShow.Seasons)
+            .GroupBy(subtitle => subtitle.Episode.TvShowId)
+            .Select(grouping => new
+            {
+                grouping.First().Episode.TvShow,
+                TotalDownloads = grouping.Sum(subtitle => subtitle.DownloadCount)
+            })
+            .OrderByDescending(arg => arg.TotalDownloads)
+            .AsNoTracking()
+            .Take(limit)
+            .ToAsyncEnumerable()
+            .Select(arg => new DownloadPopularity(arg.TvShow, arg.TotalDownloads));
     }
 
     /// <summary>
@@ -77,24 +85,24 @@ internal class ShowPopularityService : IShowPopularityService
     public IAsyncEnumerable<PopularityRecord> GetTopPopularity(int limit = 10)
     {
         return _entityContext.ShowPopularity
-                             .Include(popularity => popularity.TvShow)
-                             .Include(popularity => popularity.TvShow.Seasons)
-                             .GroupBy(popularity => popularity.TvShowId)
-                             .Select(grouping => new
-                             {
-                                 grouping.First().TvShow,
-                                 Group = grouping.ToArray(),
-                                 Sum = grouping.Sum(popularity => popularity.RequestedCount)
-                             })
-                             .OrderByDescending(group => group.Sum)
-                             .AsNoTracking()
-                             .Take(limit)
-                             .ToAsyncEnumerable()
-                             .Select(grouping =>
-                             {
-                                 var popularityCounts = grouping.Group.Select(popularity => (popularity.Language, new PopularityCount(popularity.RequestedCount, popularity.LastRequestedDate!.Value)))
-                                                                .ToImmutableDictionary(tuple => CultureInfo.GetCultureInfo(tuple.Language), tuple => tuple.Item2);
-                                 return new PopularityRecord(grouping.TvShow, popularityCounts, grouping.Sum);
-                             });
+            .Include(popularity => popularity.TvShow)
+            .Include(popularity => popularity.TvShow.Seasons)
+            .GroupBy(popularity => popularity.TvShowId)
+            .Select(grouping => new
+            {
+                grouping.First().TvShow,
+                Group = grouping.ToArray(),
+                Sum = grouping.Sum(popularity => popularity.RequestedCount)
+            })
+            .OrderByDescending(group => group.Sum)
+            .AsNoTracking()
+            .Take(limit)
+            .ToAsyncEnumerable()
+            .Select(grouping =>
+            {
+                var popularityCounts = grouping.Group.Select(popularity => (popularity.Language, new PopularityCount(popularity.RequestedCount, popularity.LastRequestedDate!.Value)))
+                    .ToImmutableDictionary(tuple => CultureInfo.GetCultureInfo(tuple.Language), tuple => tuple.Item2);
+                return new PopularityRecord(grouping.TvShow, popularityCounts, grouping.Sum);
+            });
     }
 }
