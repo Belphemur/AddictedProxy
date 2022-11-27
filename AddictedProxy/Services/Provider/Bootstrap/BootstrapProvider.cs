922d0bdfd7f9c2e9d115f9fab22f6ec1908d1206
Antoine Aflalo <Belphemur@users.noreply.github.com>
2022-11-26 23:28:58 -0500

    perf(refresher): make them scoped

--- AddictedProxy/Services/Provider/Bootstrap/BootstrapProvider.cs
+++ AddictedProxy/Services/Provider/Bootstrap/BootstrapProvider.cs
@@ -22,10 +22,10 @@ public class BootstrapProvider : IBootstrap
         services.AddJob<StoreSubtitleJob>();
         services.AddJob<FetchSubtitlesJob>();
         services.AddJob<RefreshSingleShowJob>();
-        services.AddTransient<IShowRefresher, ShowRefresher>();
+        services.AddScoped<IShowRefresher, ShowRefresher>();
         services.AddScoped<ISubtitleProvider, SubtitleProvider>();
-        services.AddTransient<ISeasonRefresher, SeasonRefresher>();
-        services.AddTransient<IEpisodeRefresher, EpisodeRefresher>();
+        services.AddScoped<ISeasonRefresher, SeasonRefresher>();
+        services.AddScoped<IEpisodeRefresher, EpisodeRefresher>();
         services.Configure<RefreshConfig>(configuration.GetSection("Refresh"));
         services.AddSingleton<IRefreshHubManager, RefreshHubManager>();
         services.AddScoped<SubtitleCounterUpdater>();
