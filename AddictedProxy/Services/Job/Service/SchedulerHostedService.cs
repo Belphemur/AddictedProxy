6e9808ffca94bb1d51a9a3f438224ec86322af12
Antoine Aflalo <Belphemur@users.noreply.github.com>
2022-12-11 19:05:16 -0500

    perf(Job): Remove from recurring job

--- AddictedProxy/Services/Job/Service/SchedulerHostedService.cs
+++ AddictedProxy/Services/Job/Service/SchedulerHostedService.cs
@@ -12,7 +12,6 @@ public class SchedulerHostedService : IHostedService
         RecurringJob.AddOrUpdate<RefreshAvailableShowsJob>(nameof(RefreshAvailableShowsJob), job => job.ExecuteAsync(CancellationToken.None), "0 2 * * *");
         RecurringJob.AddOrUpdate<CheckCompletedTmdbJob>(nameof(CheckCompletedTmdbJob), job => job.ExecuteAsync(CancellationToken.None), "50 2 * * *");
         RecurringJob.AddOrUpdate<DownloadCredsRedeemerJob>(nameof(DownloadCredsRedeemerJob), job => job.ExecuteAsync(CancellationToken.None), "50 2 * * *");
-        RecurringJob.AddOrUpdate<UpdateSubtitleLanguageJob>(nameof(UpdateSubtitleLanguageJob), job => job.ProcessAsync(default), "0 0 1 1 *");
 
         return Task.CompletedTask;
     }
