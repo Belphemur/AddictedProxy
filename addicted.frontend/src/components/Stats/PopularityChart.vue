8c174d313f7014aaa5ef3c18f6039962bf0cb977
Antoine Aflalo <Belphemur@users.noreply.github.com>
2023-01-13 21:29:21 -0500

    fix(Chart): Fix chart component for top 10

--- addicted.frontend/src/components/Stats/PopularityChart.vue
+++ addicted.frontend/src/components/Stats/PopularityChart.vue
@@ -9,8 +9,9 @@
 <script setup lang="ts">
 import { TopShowDto } from "~/api/api";
 import { computed, defineProps } from "vue";
-import PieChart from "~/components/Charts/PieChart";
+import PieChart from "~/components/Charts/PieChart.vue";
 import { stringToColour } from "~/components/Charts/generateColorFromString";
+import { ChartData } from "chart.js";
 
 interface Props {
   top: Array<TopShowDto>;
@@ -22,7 +23,7 @@ const showName = computed(() => {
   return props.top.map((value) => value.show?.name);
 });
 const showPopularity = computed(() => {
-  return props.top.map((value) => value.popularity);
+  return props.top.map((value) => value.popularity) as unknown as ChartData;
 });
 const showColor = computed(() => {
   return props.top.map((value) => stringToColour(value.show?.name ?? "N/A"));
