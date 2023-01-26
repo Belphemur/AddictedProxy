5f0a0ca8c4db53823141971155e57d2d90044719
Antoine Aflalo <Belphemur@users.noreply.github.com>
2023-01-26 17:47:02 -0500

    wip

--- AddictedProxy/Controllers/Bootstrap/BootstrapRateLimiting.cs
+++ AddictedProxy/Controllers/Bootstrap/BootstrapRateLimiting.cs
@@ -1,5 +1,7 @@
-﻿using AspNetCoreRateLimit;
+﻿using System.Threading.RateLimiting;
+using AspNetCoreRateLimit;
 using InversionOfControl.Model;
+using Microsoft.AspNetCore.RateLimiting;
 
 namespace AddictedProxy.Controllers.Bootstrap;
 
@@ -8,6 +10,24 @@ public class BootstrapRateLimiting : IBootstrap, IBootstrapApp
     public void ConfigureServices(IServiceCollection services, IConfiguration configuration)
     {
         services.AddMemoryCache();
+        services.AddRateLimiter(options =>
+        {
+            options.AddTokenBucketLimiter("default", limiterOptions =>
+            {
+                limiterOptions.ReplenishmentPeriod = TimeSpan.FromSeconds(1);
+                limiterOptions.AutoReplenishment = true;
+                limiterOptions.TokensPerPeriod = 5;
+                limiterOptions.TokenLimit = 15;
+                limiterOptions.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+            });
+            options.GlobalLimiter = PartitionedRateLimiter.Create<HttpContext, string>(context =>
+            {
+                return new RateLimitPartition<string>(context.Request.Headers["CF-Connecting-IP"].FirstOrDefault() ?? "default", ip =>
+                {
+                    return new TokenBucketRateLimiter()
+                } );
+            });
+        });
         services.Configure<IpRateLimitOptions>(configuration.GetSection("IpRateLimiting"));
 
         //load ip rules from appsettings.json
@@ -20,5 +40,9 @@ public class BootstrapRateLimiting : IBootstrap, IBootstrapApp
     public void ConfigureApp(IApplicationBuilder app)
     {
         app.UseIpRateLimiting();
+        app.UseRateLimiter(new RateLimiterOptions
+        {
+            RejectionStatusCode = 429
+        });
     }
 }
