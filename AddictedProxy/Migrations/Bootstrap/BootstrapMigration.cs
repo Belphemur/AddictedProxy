ac84ea98bbe5c07482e7f3530f9491583617915c
Antoine Aflalo <Belphemur@users.noreply.github.com>
2023-02-28 19:09:57 -0500

    feat(tvdb): Populate the tvdb ids for existing shows

---
+++ AddictedProxy/Migrations/Bootstrap/BootstrapMigration.cs
@@ -0,0 +1,13 @@
+﻿using AddictedProxy.Migrations.Services;
+using AddictedProxy.OneTimeMigration.Model;
+using InversionOfControl.Model;
+
+namespace AddictedProxy.Migrations.Bootstrap;
+
+public class BootstrapMigration : IBootstrap
+{
+    public void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddScoped<IMigration, PopulateTvDbIdsMigration>();
+    }
+}
