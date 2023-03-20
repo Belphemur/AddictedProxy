586ec7d02496b72227c97579ac5e25036e034c26
Antoine Aflalo <Belphemur@users.noreply.github.com>
2023-03-20 17:56:36 -0400

    feat(Privacy): Add privacy policy to the website

---
+++ addicted.frontend/src/views/Privacy.vue
@@ -0,0 +1,76 @@
+<template>
+  <el-row :gutter="20">
+    <el-col :span="18" :offset="3">
+      <el-card class="policy" header="Privacy Policy">
+        <p>
+          <span class="subtext">
+            Updated date: March 20, 2023 <br /><br
+          /></span>
+          This Privacy Policy explains how gestdown.info ("we" or "us")
+          collects, uses, and discloses information about visitors to our
+          website. By using our website, you agree to the terms of this Privacy
+          Policy.
+        </p>
+        <h2>1. Information We Collect</h2>
+        <p>
+          We do not collect any personal information from our visitors. We do
+          not log the IP addresses of our visitors or collect any other
+          information that could be used to identify individuals. We do not
+          require any account registration to use our services.
+        </p>
+        <h2>2. Use of Cookies</h2>
+        <p>
+          We do not use cookies or other tracking technologies on our website.
+        </p>
+        <h2>3. Third-Party Links</h2>
+        <p>
+          Our website may contain links to third-party websites that are not
+          operated by us. We are not responsible for the privacy practices of
+          these websites. We encourage you to review the privacy policies of
+          these websites before using them.
+        </p>
+        <h2>4. Children's Privacy</h2>
+        <p>
+          Our website is not directed to children under the age of 13. We do not
+          knowingly collect personal information from children under the age of
+          13.
+        </p>
+        <h2>5. Information Sharing</h2>
+        <p>
+          We do not share any visitor information with third-party websites or
+          services, including Addic7ed.
+        </p>
+        <h2>6. Metrics Collection</h2>
+        <p>
+          We aggregate the received requests to provide metrics used to monitor
+          the health of the system. This data is not linked to any personal
+          information and is used only to maintain the quality and availability
+          of our services.
+        </p>
+        <h2>7. Changes to this Privacy Policy</h2>
+        <p>
+          We reserve the right to modify this Privacy Policy at any time. If we
+          make material changes to this Privacy Policy, we will notify you by
+          posting the changes on our website.
+        </p>
+        <h2>8. Contact Us</h2>
+        <p>
+          If you have any questions about this Privacy Policy, please contact us
+          at support@gestdown.info.
+        </p>
+      </el-card>
+    </el-col>
+  </el-row>
+</template>
+
+<script lang="ts" setup></script>
+
+<style scoped>
+.policy {
+  text-align: justify;
+}
+
+.subtext {
+  font-size: small;
+}
+</style>
