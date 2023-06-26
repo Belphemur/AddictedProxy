e390e5f02d56c7b50be7ba9469dd1efdd6b84377
Antoine Aflalo <Belphemur@users.noreply.github.com>
2023-06-26 12:12:18 -0400

    refactor: basic scafolding

--- addicted.vuetify/src/layouts/default/Default.vue
+++ addicted.vuetify/src/layouts/default/Default.vue
@@ -1,5 +1,6 @@
 <template>
-  <v-app>
+  <v-app :style="defaultBackground">
+    <div class="bg-image" :style="backgroundStyle"></div>
     <default-bar v-on:drawer-clicked="drawer = !drawer"/>
 
     <navigation v-model="drawer" />
@@ -14,4 +15,34 @@
   import Navigation from "@/layouts/default/Navigation.vue";
   import {ref} from "vue";
   const drawer = ref(true)
+
+  import background from "@/assets/background.webp";
+
+  const backgroundStyle = {
+    backgroundImage: `url(${background})`,
+  };
+  const defaultBackground = {
+    background: "none",
+  }
 </script>
+<style scoped>
+.bg-image {
+  /* Add the blur effect */
+  filter: blur(8px);
+  -webkit-filter: blur(8px);
+
+  /* Full height */
+  height: 100vh;
+  width: 100%;
+  z-index: -1;
+  position: fixed;
+  left: 0;
+  right: 0;
+  display: block;
+
+  /* Center and scale the image nicely */
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+</style>
