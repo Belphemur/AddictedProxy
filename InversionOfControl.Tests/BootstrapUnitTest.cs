8fa20c734e55df69b5e502c9da29e4b1842c8968
Antoine Aflalo <Belphemur@users.noreply.github.com>
2023-08-19 14:01:54 -0400

    test: add test for factory

--- InversionOfControl.Tests/BootstrapUnitTest.cs
+++ InversionOfControl.Tests/BootstrapUnitTest.cs
@@ -1,13 +1,14 @@
 using FluentAssertions;
 using InversionOfControl.Model;
+using InversionOfControl.Model.Factory;
 using InversionOfControl.Service.Bootstrap;
 using InversionOfControl.Tests.Mock;
+using InversionOfControl.Tests.Mock.Factory;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using NSubstitute;
 
 namespace InversionOfControl.Tests;
-
 
 public class Tests
 {
@@ -21,7 +22,7 @@ public class Tests
         var baseService = serviceProvider.GetRequiredService<IBaseService>();
         baseService.Name.Should().Be("Test");
     }
-    
+
     [Test]
     public void TestRegistrationOfServiceConditional()
     {
@@ -30,7 +31,21 @@ public class Tests
         bootstrapRegister.RegisterBootstrapServices(serviceCollection, Substitute.For<IConfiguration>(), typeof(BoostrapServiceMock).Assembly);
         var serviceProvider = serviceCollection.BuildServiceProvider();
         var baseService = serviceProvider.GetService<BootstrapServiceMockConditional.IShouldntExists>();
-        
+
         baseService.Should().BeNull();
+    }
+
+    [Test]
+    public void TestFactoryRegistration()
+    {
+        var bootstrapRegister = new BootstrapRegister();
+        var serviceCollection = new ServiceCollection();
+        bootstrapRegister.RegisterBootstrapServices(serviceCollection, Substitute.For<IConfiguration>(), typeof(BoostrapServiceMock).Assembly);
+        var serviceProvider = serviceCollection.BuildServiceProvider();
+        var baseService = serviceProvider.GetRequiredService<EnumFactory<FactType, IFactoryServiceMock>>();
+        baseService.Should().NotBeNull();
+        baseService.GetService(FactType.Foo).Should().BeOfType<FooService>();
+        baseService.GetService(FactType.Test).Should().BeOfType<TestService>();
+
     }
 }
