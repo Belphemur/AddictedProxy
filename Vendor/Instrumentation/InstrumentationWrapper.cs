16dbb670df6722349d9fc91099b67db9b0f92023
Antoine Aflalo <Belphemur@users.noreply.github.com>
2023-08-23 16:00:17 -0400

    refactor: cleanup

--- Vendor/Instrumentation/InstrumentationWrapper.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Diagnostics;
-
-namespace NeoSmart.Caching.Sqlite.Instrumentation;
-
-public class InstrumentationWrapper
-{
-    public static readonly ActivitySource ActivitySource = new("SqlCache", typeof(InstrumentationWrapper).Assembly.GetName().Version?.ToString() ?? "1.0.0");
-}
