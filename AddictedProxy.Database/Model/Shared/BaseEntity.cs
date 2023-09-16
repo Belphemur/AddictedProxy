90547212741391b2252f1de2fac3057f983b7de7
Antoine Aflalo <Belphemur@users.noreply.github.com>
2023-09-16 13:14:32 -0400

    feat: add support for auto created/updated fields

---
+++ AddictedProxy.Database/Model/Shared/BaseEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace AddictedProxy.Database.Model.Shared;
+
+public abstract class BaseEntity
+{
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+}
