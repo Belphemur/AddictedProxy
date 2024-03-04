bb51b7d496c0c7b236c5cd1c16785634a45b46cd
Antoine Aflalo <Belphemur@users.noreply.github.com>
2024-03-04 09:35:04 -0500

    perf(deps): update deps

--- AddictedProxy/Utils/ResultExtensions.cs
+++ AddictedProxy/Utils/ResultExtensions.cs
@@ -6,51 +6,63 @@ public static class ResultExtensions
 {
     public static async Task<Result<TDestination>> MapAsync<TSource, TDestination>(
         this Result<TSource> result,
-        Func<TSource, Task<Result<TDestination>>> func)
+        Func<TSource, Task<Result<TDestination>>> func
+    )
     {
         return result.Status switch
         {
-            ResultStatus.Ok           => await func((TSource)result),
-            ResultStatus.Error        => Result<TDestination>.Error(result.Errors.ToArray<string>()),
-            ResultStatus.Forbidden    => Result<TDestination>.Forbidden(),
+            ResultStatus.Ok => await func((TSource)result),
+            ResultStatus.Error => Result<TDestination>.Error(result.Errors.ToArray<string>()),
+            ResultStatus.Forbidden => Result<TDestination>.Forbidden(),
             ResultStatus.Unauthorized => Result<TDestination>.Unauthorized(),
-            ResultStatus.Invalid      => Result<TDestination>.Invalid(result.ValidationErrors),
-            ResultStatus.NotFound     => Result<TDestination>.NotFound(),
-            _                         => throw new NotSupportedException()
+            ResultStatus.Invalid => Result<TDestination>.Invalid(result.ValidationErrors.ToArray()),
+            ResultStatus.NotFound => Result<TDestination>.NotFound(),
+            ResultStatus.Conflict => Result<TDestination>.Conflict(result.Errors.ToArray<string>()),
+            ResultStatus.CriticalError => Result<TDestination>.CriticalError(result.Errors.ToArray<string>()),
+            ResultStatus.Unavailable => Result<TDestination>.Unavailable(),
+            _ => throw new NotSupportedException()
         };
     }
 
     public static async Task<Result<TDestination>> MapAsync<TSource, TDestination>(
         this Task<Result<TSource>> taskResult,
-        Func<TSource, Task<Result<TDestination>>> func)
+        Func<TSource, Task<Result<TDestination>>> func
+    )
     {
         var result = await taskResult;
         return result.Status switch
         {
-            ResultStatus.Ok           => await func((TSource)result),
-            ResultStatus.Error        => Result<TDestination>.Error(result.Errors.ToArray<string>()),
-            ResultStatus.Forbidden    => Result<TDestination>.Forbidden(),
+            ResultStatus.Ok => await func((TSource)result),
+            ResultStatus.Error => Result<TDestination>.Error(result.Errors.ToArray<string>()),
+            ResultStatus.Forbidden => Result<TDestination>.Forbidden(),
             ResultStatus.Unauthorized => Result<TDestination>.Unauthorized(),
-            ResultStatus.Invalid      => Result<TDestination>.Invalid(result.ValidationErrors),
-            ResultStatus.NotFound     => Result<TDestination>.NotFound(),
-            _                         => throw new NotSupportedException()
+            ResultStatus.Invalid => Result<TDestination>.Invalid(result.ValidationErrors.ToArray()),
+            ResultStatus.NotFound => Result<TDestination>.NotFound(),
+            ResultStatus.Conflict => Result<TDestination>.Conflict(result.Errors.ToArray<string>()),
+            ResultStatus.CriticalError => Result<TDestination>.CriticalError(result.Errors.ToArray<string>()),
+            ResultStatus.Unavailable => Result<TDestination>.Unavailable(),
+            _ => throw new NotSupportedException()
         };
     }
 
     public static async Task<Result<TDestination>> MapAsync<TSource, TDestination>(
         this Task<Result<TSource>> taskResult,
-        Func<TSource, TDestination> func)
+        Func<TSource, TDestination> func
+    )
     {
         var result = await taskResult;
         return result.Status switch
         {
-            ResultStatus.Ok           => func((TSource)result),
-            ResultStatus.Error        => Result<TDestination>.Error(result.Errors.ToArray<string>()),
-            ResultStatus.Forbidden    => Result<TDestination>.Forbidden(),
+            ResultStatus.Ok => func((TSource)result),
+            ResultStatus.Error => Result<TDestination>.Error(result.Errors.ToArray<string>()),
+            ResultStatus.Forbidden => Result<TDestination>.Forbidden(),
             ResultStatus.Unauthorized => Result<TDestination>.Unauthorized(),
-            ResultStatus.Invalid      => Result<TDestination>.Invalid(result.ValidationErrors),
-            ResultStatus.NotFound     => Result<TDestination>.NotFound(),
-            _                         => throw new NotSupportedException()
+            ResultStatus.Invalid => Result<TDestination>.Invalid(result.ValidationErrors.ToArray()),
+            ResultStatus.NotFound => Result<TDestination>.NotFound(),
+            ResultStatus.Conflict => Result<TDestination>.Conflict(result.Errors.ToArray<string>()),
+            ResultStatus.CriticalError => Result<TDestination>.CriticalError(result.Errors.ToArray<string>()),
+            ResultStatus.Unavailable => Result<TDestination>.Unavailable(),
+            _ => throw new NotSupportedException()
         };
     }
 }
