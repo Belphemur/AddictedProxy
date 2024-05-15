65902b74ff0b2280a5bc21567e0dbd732a3a0fed
Antoine Aflalo <197810+Belphemur@users.noreply.github.com>
2024-05-15 17:05:38 -0400

    perf(sentry): set distributed tracing

--- addicted.nuxt/composables/rest/api.ts
+++ addicted.nuxt/composables/rest/api.ts
@@ -2,6 +2,7 @@ import {Subtitles} from "~/composables/api/Subtitles";
 import {Shows} from "~/composables/api/Shows";
 import {Media} from "~/composables/api/Media";
 import type {ApiConfig} from "~/composables/api/http-client";
+import * as Sentry from '@sentry/vue'
 
 
 let subtitles: Subtitles<any>;
@@ -10,20 +11,46 @@ let media: Media<any>;
 
 export function getApiServerUrl(): string {
     const config = useRuntimeConfig();
-    return typeof window === "undefined" ? config.public.api.serverUrl : config.public.api.clientUrl ;
+    return typeof window === "undefined" ? config.public.api.serverUrl : config.public.api.clientUrl;
 }
 
 function getApiConfig() {
-    const apiConfig : ApiConfig = {
+    const apiConfig: ApiConfig = {
         // @ts-ignore
         baseUrl: getApiServerUrl(),
-        customFetch: input => {
-            console.log("fetching", input)
-            if(input.credentials !== undefined)
-               delete input.credentials;
-            if(input.mode !== undefined)
-                delete input.mode;
-            return fetch(input)
+        customFetch: (input, init) => {
+            //Clean up fetch request for server request of worker
+            if (typeof window === "undefined") {
+                console.log("fetching", input)
+                if (input.credentials !== undefined)
+                    delete input.credentials;
+                if (input.mode !== undefined)
+                    delete input.mode;
+            }
+            const activeSpan = Sentry.getActiveSpan();
+            const rootSpan = activeSpan ? Sentry.getRootSpan(activeSpan) : undefined;
+
+// Create `sentry-trace` header
+            const sentryTraceHeader = rootSpan ? Sentry.spanToTraceHeader(rootSpan) : undefined;
+
+// Create `baggage` header
+            const sentryBaggageHeader = rootSpan ? Sentry.spanToBaggageHeader(rootSpan) : "undefined";
+
+            let addedHeader = {}
+            if (sentryBaggageHeader !== undefined && sentryTraceHeader !== undefined) {
+                addedHeader = {
+                    baggage: sentryBaggageHeader,
+                    'sentry-trace': sentryTraceHeader,
+                }
+            }
+            init = {
+                ...init,
+                headers: {
+                    ...init?.headers,
+                    ...addedHeader
+                },
+            };
+            return fetch(input, init)
         },
     };
     return apiConfig;
