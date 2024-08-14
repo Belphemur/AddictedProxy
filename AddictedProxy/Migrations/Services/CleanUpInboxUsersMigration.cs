955c44cb868c5a7f225cc0da0c5cd5965648cbf3
Antoine Aflalo <197810+Belphemur@users.noreply.github.com>
2024-08-14 17:54:24 -0400

    perf: add logging

--- AddictedProxy/Migrations/Services/CleanUpInboxUsersMigration.cs
+++ AddictedProxy/Migrations/Services/CleanUpInboxUsersMigration.cs
@@ -8,11 +8,13 @@ public class CleanUpInboxUsersMigration : IMigration
 {
     private readonly IAddic7edClient _addic7EdClient;
     private readonly IAddictedUserCredentialRepository _credentialRepository;
+    private readonly ILogger<CleanUpInboxUsersMigration> _logger;
 
-    public CleanUpInboxUsersMigration(IAddic7edClient addic7EdClient, IAddictedUserCredentialRepository credentialRepository)
+    public CleanUpInboxUsersMigration(IAddic7edClient addic7EdClient, IAddictedUserCredentialRepository credentialRepository, ILogger<CleanUpInboxUsersMigration> logger)
     {
         _addic7EdClient = addic7EdClient;
         _credentialRepository = credentialRepository;
+        _logger = logger;
     }
 
     public async Task ExecuteAsync(CancellationToken token)
@@ -20,7 +22,10 @@ public class CleanUpInboxUsersMigration : IMigration
         var creds = await _credentialRepository.GetAllCredentialsAsync().ToArrayAsync(token);
         foreach (var cred in creds)
         {
-            await _addic7EdClient.CleanupInbox(cred, token);
+            if (!await _addic7EdClient.CleanupInbox(cred, token))
+            {
+                _logger.LogWarning("Couldn't clean up inbox for {CredentialId}", cred.Id);
+            }
             cred.DownloadExceededDate = null;
             cred.Usage = 0;
             cred.DownloadUsage = 0;
