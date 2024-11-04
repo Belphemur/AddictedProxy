2a3811e2417ffbd54fc826110cfa261fc31d5600
Antoine Aflalo <197810+Belphemur@users.noreply.github.com>
2024-11-04 11:48:01 -0500

    fix(result): remove unneeded extension
    Added by the main lib

--- AddictedProxy/Utils/ResultExtensions.cs
+++ AddictedProxy/Utils/ResultExtensions.cs
@@ -45,24 +45,5 @@ public static class ResultExtensions
         };
     }
 
-    public static async Task<Result<TDestination>> MapAsync<TSource, TDestination>(
-        this Task<Result<TSource>> taskResult,
-        Func<TSource, TDestination> func
-    )
-    {
-        var result = await taskResult;
-        return result.Status switch
-        {
-            ResultStatus.Ok => func((TSource)result),
-            ResultStatus.Error => Result<TDestination>.CriticalError(result.Errors.ToArray<string>()),
-            ResultStatus.Forbidden => Result<TDestination>.Forbidden(),
-            ResultStatus.Unauthorized => Result<TDestination>.Unauthorized(),
-            ResultStatus.Invalid => Result<TDestination>.Invalid(result.ValidationErrors.ToArray()),
-            ResultStatus.NotFound => Result<TDestination>.NotFound(),
-            ResultStatus.Conflict => Result<TDestination>.Conflict(result.Errors.ToArray<string>()),
-            ResultStatus.CriticalError => Result<TDestination>.CriticalError(result.Errors.ToArray<string>()),
-            ResultStatus.Unavailable => Result<TDestination>.Unavailable(),
-            _ => throw new NotSupportedException()
-        };
-    }
+ 
 }
