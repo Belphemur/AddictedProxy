282b9fa9070d9dd99d07fa3491a135dbb2bce50e
Antoine Aflalo <197810+Belphemur@users.noreply.github.com>
2025-02-05 09:04:29 -0500

    fix(performance): register a fake performance tracker for None

---
+++ Performance/Bootstrap/BootstrapNone.cs
@@ -0,0 +1,23 @@
+﻿using InversionOfControl.Model;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Performance.Model;
+using Performance.Service;
+using Performance.Service.None;
+
+namespace Performance.Bootstrap;
+
+public class BootstrapNone : IBootstrapConditional
+{
+    public void ConfigureServices(IServiceCollection services, IConfiguration configuration, ILoggingBuilder logging)
+    {
+        services.AddSingleton<IPerformanceTracker, PerformanceTrackerNone>();
+    }
+
+    public bool ShouldLoadBootstrap(IConfiguration configuration)
+    {
+        var perf = configuration.GetSection("Performance").Get<PerformanceConfig>()!;
+        return perf.Type == PerformanceConfig.BackendType.None;
+    }
+}
