2c9e84b0a6f62318842918cf6c6ff6d0cde98182
Antoine Aflalo <197810+Belphemur@users.noreply.github.com>
2026-02-19 20:23:08 -0500

    feat(migrations): backport Addic7ed HD flag to VideoQuality bitfield
    Add BackportHdToQualitiesMigration (IMigration, date 2026-02-19) that
    batch-updates existing Addic7ed subtitles where HD = true, setting
    Qualities = Q720P | Q1080P (bitmask 12). Runs via the existing
    one-time migration runner on startup.

--- AddictedProxy.Database/Model/Shows/VideoQuality.cs
+++ AddictedProxy.Database/Model/Shows/VideoQuality.cs
@@ -7,10 +7,10 @@ namespace AddictedProxy.Database.Model.Shows;
 [Flags]
 public enum VideoQuality
 {
-    None   = 0,
-    Q360P  = 1 << 0,  // 1
-    Q480P  = 1 << 1,  // 2
-    Q720P  = 1 << 2,  // 4
+    None = 0,
+    Q360P = 1 << 0,  // 1
+    Q480P = 1 << 1,  // 2
+    Q720P = 1 << 2,  // 4
     Q1080P = 1 << 3,  // 8
     Q2160P = 1 << 4,  // 16
 }
